134406ffaf7872399ebb353bd7f61ca0fe99118f
Aralyne Gonçalves <aralynegs@gmail.com>
2020-12-31 15:54:41 -0300

    Configuração do .env e correções de config Twitter e open_weather_map

--- app/adapters/open_weather_map/config.rb
+++ app/adapters/open_weather_map/config.rb
@@ -1,6 +1,6 @@
 class OpenWeatherMap::Config
   def initialize(city_id, endpoint)
-    @api_key = 'e88c1515be1de476a1469d4bdb9cb1eb'
+    @api_key = ENV['OPEN_WEATHER_MAP']
     @city_id = city_id
     @endpoint = endpoint
     @api_response = HTTParty.get("http://api.openweathermap.org/data/2.5/#{@endpoint}?id=#{@city_id}&units=metric&appid=#{@api_key}&lang=pt_br")
--- app/adapters/twitter/config.rb
+++ app/adapters/twitter/config.rb
@@ -1,10 +1,10 @@
 class Twitter::Config
   def initialize
       @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "O2XqAd2sZ4rX795BNJQIW8N2k"
-      config.consumer_secret     = "ekLMPHndJ5gF4evmIDe8zJThOt4VYlZDr9JEZwx6UgzFa4FLNn"
-      config.access_token        = "274830132-i6nvdvFvsR583rJjROAOFRlZYae5zLKafhpp93UR"
-      config.access_token_secret = "ApXeptlfnW8hHKbxxtkjlwuoIs3bYcy64oQRKSGKmGWjE"
+      config.consumer_key        = ENV['CONSUMER_KEY'] 
+      config.consumer_secret     = ENV['CONSUMER_SECRET'] 
+      config.access_token        = ENV['ACCESS_TOKEN'] 
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
       end
   end
 
